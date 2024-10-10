4ac50655a0378e9204c307b982710cbcbee7f26b
Victor Revenco <victor.revenco@isa.utm.md>
2024-10-10 17:27:41 +0300

    Add validation for products

--- Lab1/src/main/java/ProductValidator.java
+++ Lab1/src/main/java/ProductValidator.java
@@ -1,2 +1,15 @@
 public class ProductValidator {
+    public String cleanString(String input) {
+        return input.trim();
+    }
+
+    public Integer validatePrice(String priceString) {
+        try {
+            priceString = priceString.trim();
+            return Integer.parseInt(priceString);
+        } catch (NumberFormatException e) {
+            System.out.println("Price is not a valid integer: " + priceString);
+            return null;
+        }
+    }
 }
